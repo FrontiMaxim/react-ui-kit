8bc048d8f12369b117f916840dd44000241ac4b9
Rustorguev Maxim <frontimax@yandex.ru>
2023-09-14 18:08:41 +0400

    feat: add toast notification element

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import styles from './App.module.scss';
+import ToastNotification from './components/ToastNotification/ToastNotification';
 
 const App = () => {
   return (
     <div className={styles.app}>
-       
+      <ToastNotification 
+        content='Операция прошла успешно freferferf gtrgerherh fregwrtghrwt ghtrwgh fergwerwg grewgwr' 
+        place='right-down'
+      />
     </div>
   );
 }
